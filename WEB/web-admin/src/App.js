6c138e849378dab62a17b7b87ce1ec6b7c0bceb9
Rujang <89078451+Rujang@users.noreply.github.com>
2021-09-22 15:04:48 +0000

    first-commit

---
+++ WEB/web-admin/src/App.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Admin,Resource } from 'react-admin';
+import { UserList } from "./users";
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+  <Admin dataProvider={dataProvider} >
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
+
+
+export default App;
---
+++ WEB/web-admin/src/users.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { List, Datagrid, TextField, NumberField, ImageField } from 'react-admin';
+
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField label="순번" source="id" />
+            <ImageField label="사진" source="picture" />
+            <TextField label="계급" source="id" />
+            <TextField label="이름" source="name" />
+            <NumberField label="군번" source="score" />
+            <span>567</span>
+            <TextField label="소속" source="company.name" />
+        </Datagrid>
+
+    </List>
+);
