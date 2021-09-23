1a0f3c756fdfcd13ba5fb2f3e2121fe2a71e65f4
Rujang <89078451+Rujang@users.noreply.github.com>
2021-09-23 14:16:29 +0000

    user tab add

--- WEB/web-admin/src/App.js
+++ WEB/web-admin/src/App.js
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { Admin,Resource } from 'react-admin';
-import { UserList } from "./users";
+import { UserList, UserEdit, UserCreate } from "./users";
 import { NotUserList } from "./notusers";
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider} >
-    <Resource name="users" list={UserList} />
+    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} />
     <Resource name="posts" list={NotUserList} />
   </Admin>
 );
--- WEB/web-admin/src/users.js
+++ WEB/web-admin/src/users.js
@@ -1,15 +1,56 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ImageField } from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    ImageField,
+    DateField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+} from 'react-admin';
 
 export const UserList = props => (
     <List {...props}>
-        <Datagrid rowClick="edit">
+        <Datagrid>
             <TextField label="순번" source="id" />
-            <ImageField label="사진" />
-            <TextField label="계급" />
+            <ImageField label="사진" source="address.suite" />
+            <TextField label="계급" source="address.street" />
             <TextField label="이름" source="name" />
-            <TextField label="군번" />
+            <TextField label="군번" source="phone" />
             <TextField label="소속" source="company.name" />
+            <EditButton />
         </Datagrid>
     </List>
+);
+
+export const UserEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput label="순번" source="id" />
+            <ImageField label="사진" source="address.suite" />
+            <TextInput label="계급" source="address.street" />
+            <TextInput label="이름" source="name" />
+            <TextInput label="군번" source="phone" />
+            <TextInput label="소속" source="company.name" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const UserCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput label="순번" source="id" />
+            <ImageField label="사진" source="address.suite" />
+            <TextInput label="계급" source="address.street" />
+            <TextInput label="이름" source="name" />
+            <TextInput label="군번" source="phone" />
+            <TextInput label="소속" source="company.name" />
+        </SimpleForm>
+    </Create>
 );
