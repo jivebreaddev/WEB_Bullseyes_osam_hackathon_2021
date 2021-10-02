1e00b1d56c728b5df68c56dbc1bf16a720125d54
jivebreaddev <89639413+jivebreaddev@users.noreply.github.com>
2021-10-02 08:07:37 +0000

    model completed

--- APP/bullseyes/bullseyes_server/models.py
+++ APP/bullseyes/bullseyes_server/models.py
@@ -2,4 +2,18 @@ from django.db import models
 
 # Create your models here.
 class User:
-    pass
+    id = models.BigAutoField(primary_key=True)
+    photourl = models.CharField(max_length=100, blank=True)
+    rank = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=100, blank=True)
+    altid = models.CharField(max_length=100, blank=True)
+    company = models.CharField(max_length=100, blank=True)
+class AccessUser:
+    id = models.BigAutoField(primary_key=True)
+    photourl = models.CharField(max_length=100, blank=True)
+    place = models.CharField(max_length=100, blank=True)
+    time = models.CharField(blank=False, max_length=100)
+    rank = models.CharField(blank=False, max_length=100)
+    name = models.CharField(blank=False, max_length=100)
+    altid = models.CharField(blank=False, max_length=100)    
+    company = models.CharField(blank=False, max_length=100)    
