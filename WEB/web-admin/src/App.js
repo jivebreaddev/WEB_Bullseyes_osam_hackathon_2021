5c7dd9781a9dde7a4d970e09b4005b9913e62f22
jivebreaddev <89639413+jivebreaddev@users.noreply.github.com>
2021-10-04 01:10:48 +0000

    prettier format applied

--- WEB/web-admin/src/App.js
+++ WEB/web-admin/src/App.js
@@ -1,25 +1,45 @@
 import * as React from "react";
-import { Admin,Resource } from 'react-admin';
+import { Admin, Resource } from "react-admin";
 import { UserList, UserEdit, UserCreate, UserShow } from "./users";
 import { NotUserList } from "./notusers";
-import jsonServerProvider from 'ra-data-json-server';
-import UserIcon from '@material-ui/icons/Group';
-import NotUserIcon from '@material-ui/icons/Warning';
-import AccessUserIcon from '@material-ui/icons/ContactMail';
+import jsonServerProvider from "ra-data-json-server";
+import UserIcon from "@material-ui/icons/Group";
+import NotUserIcon from "@material-ui/icons/Warning";
+import AccessUserIcon from "@material-ui/icons/ContactMail";
 import Dashboard from "./Dashboard";
 import authProvider from "./authProvider";
 import { AccessUserList } from "./AccessUser";
 import UserStatistics from "./UserStatistics";
 import faceRecognition from "./faceRecognition";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} >
-    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} show={UserShow} icon={UserIcon} options={{ label: '사용자' }} />
-    <Resource name="comments" list={AccessUserList} icon={AccessUserIcon} options={{ label: '출입한 사용자' }} />
-    <Resource name="statistics" list={UserStatistics} options={{ label: '통계' }} />
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
+    <Resource
+      name="users"
+      list={UserList}
+      edit={UserEdit}
+      create={UserCreate}
+      show={UserShow}
+      icon={UserIcon}
+      options={{ label: "사용자" }}
+    />
+    <Resource
+      name="comments"
+      list={AccessUserList}
+      icon={AccessUserIcon}
+      options={{ label: "출입한 사용자" }}
+    />
+    <Resource
+      name="statistics"
+      list={UserStatistics}
+      options={{ label: "통계" }}
+    />
   </Admin>
 );
-
 
 export default App;
--- WEB/web-admin/src/users.js
+++ WEB/web-admin/src/users.js
@@ -1,78 +1,78 @@
 import * as React from "react";
 import {
-    List,
-    Datagrid,
-    TextField,
-    ImageField,
-    EditButton,
-    Edit,
-    Create,
-    SimpleForm,
-    SelectInput,
-    TextInput,
-    Show,
-    SimpleShowLayout,
-} from 'react-admin';
+  List,
+  Datagrid,
+  TextField,
+  ImageField,
+  EditButton,
+  Edit,
+  Create,
+  SimpleForm,
+  SelectInput,
+  TextInput,
+  Show,
+  SimpleShowLayout,
+} from "react-admin";
 
 const choices = [
-    { id: 'Leanne Graham', name: 'Leanne Graham' },
-    { id: 'gg', name: 'gg' },
+  { id: "Leanne Graham", name: "Leanne Graham" },
+  { id: "gg", name: "gg" },
 ];
 
 const userFilters = [
-    <TextInput label="Search" source="q" alwaysOn />,
-    <SelectInput source="name" choices={choices} />,
+  <TextInput label="Search" source="q" alwaysOn />,
+  <SelectInput source="name" choices={choices} />,
 ];
 
-export const UserList = props => (
-    <List filters={userFilters} {...props}>
-        <Datagrid rowClick="show">
-            <TextField label="순번" source="id" />
-            <ImageField label="사진" source="address.suite" />
-            <TextField label="계급" source="address.street" />
-            <TextField label="이름" source="name" />
-            <TextField label="군번" source="phone" />
-            <TextField label="소속" source="company.name" />
-            <EditButton />
-        </Datagrid>
-    </List>
+export const UserList = (props) => (
+  <List filters={userFilters} {...props}>
+    <Datagrid rowClick="show">
+      <TextField label="순번" source="id" />
+      <ImageField label="사진" source="address.suite" />
+      <TextField label="계급" source="address.street" />
+      <TextField label="이름" source="name" />
+      <TextField label="군번" source="phone" />
+      <TextField label="소속" source="company.name" />
+      <EditButton />
+    </Datagrid>
+  </List>
 );
 
-export const UserEdit = props => (
-    <Edit {...props}>
-        <SimpleForm>
-            <TextInput label="순번" source="id" />
-            <ImageField label="사진" source="address.suite" />
-            <TextInput label="계급" source="address.street" />
-            <TextInput label="이름" source="name" />
-            <TextInput label="군번" source="phone" />
-            <TextInput label="소속" source="company.name" />
-        </SimpleForm>
-    </Edit>
+export const UserEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput label="순번" source="id" />
+      <ImageField label="사진" source="address.suite" />
+      <TextInput label="계급" source="address.street" />
+      <TextInput label="이름" source="name" />
+      <TextInput label="군번" source="phone" />
+      <TextInput label="소속" source="company.name" />
+    </SimpleForm>
+  </Edit>
 );
 
-export const UserCreate = props => (
-    <Create {...props}>
-        <SimpleForm>
-            <TextInput label="순번" source="id" />
-            <ImageField label="사진" source="address.suite" />
-            <TextInput label="계급" source="address.street" />
-            <TextInput label="이름" source="name" />
-            <TextInput label="군번" source="phone" />
-            <TextInput label="소속" source="company.name" />
-        </SimpleForm>
-    </Create>
+export const UserCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput label="순번" source="id" />
+      <ImageField label="사진" source="address.suite" />
+      <TextInput label="계급" source="address.street" />
+      <TextInput label="이름" source="name" />
+      <TextInput label="군번" source="phone" />
+      <TextInput label="소속" source="company.name" />
+    </SimpleForm>
+  </Create>
 );
 
 export const UserShow = (props) => (
-    <Show {...props}>
-        <SimpleShowLayout>
-            <TextField label="순번" source="id" />
-            <ImageField label="사진" source="address.suite" />
-            <TextField label="계급" source="address.street" />
-            <TextField label="이름" source="name" />
-            <TextField label="군번" source="phone" />
-            <TextField label="소속" source="company.name" />
-        </SimpleShowLayout>
-    </Show>
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField label="순번" source="id" />
+      <ImageField label="사진" source="address.suite" />
+      <TextField label="계급" source="address.street" />
+      <TextField label="이름" source="name" />
+      <TextField label="군번" source="phone" />
+      <TextField label="소속" source="company.name" />
+    </SimpleShowLayout>
+  </Show>
 );
